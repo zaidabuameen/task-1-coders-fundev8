f08fb9424371bcae2999f09fe7ee06f047e91cd8
zaidabuameen <zaidaboameen8@gmail.com>
2025-09-16 22:33:26 +0300

    Initial commit of project files

---
+++ js learn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace js_learn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
